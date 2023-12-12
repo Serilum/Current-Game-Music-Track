c276885ec0f30a1e3e2184cb783e6f0660801ca5
Rick South <rick@natamus.com>
2023-12-12 10:35:39 +0100

    Current Game Music Track source code for 1.20.4.

---
+++ Common/src/main/java/com/natamus/currentgamemusictrack/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.currentgamemusictrack;
+
+import com.natamus.currentgamemusictrack.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
