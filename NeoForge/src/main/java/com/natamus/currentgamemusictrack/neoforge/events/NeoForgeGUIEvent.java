190305dbb0d2d9225bf91506cb88df7f1ea49567
Rick South <rick@natamus.com>
2024-04-28 19:03:06 +0200

    (v1.5) (1.20.5 Neo) Updated to work with NeoForge's TickEvent changes in 20.5.14.

--- NeoForge/src/main/java/com/natamus/currentgamemusictrack/neoforge/events/NeoForgeGUIEvent.java
+++ NeoForge/src/main/java/com/natamus/currentgamemusictrack/neoforge/events/NeoForgeGUIEvent.java
@@ -3,11 +3,11 @@ package com.natamus.currentgamemusictrack.neoforge.events;
 import com.natamus.currentgamemusictrack.data.Constants;
 import com.natamus.currentgamemusictrack.events.GUIEvent;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.neoforge.client.event.RenderGuiEvent;
-import net.neoforged.neoforge.event.TickEvent;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.client.event.RenderGuiEvent;
 
 @EventBusSubscriber(value = Dist.CLIENT)
 public class NeoForgeGUIEvent {
@@ -17,7 +17,7 @@ public class NeoForgeGUIEvent {
 	}
 
 	@SubscribeEvent
-	public static void onClientTick(TickEvent.ClientTickEvent e) {
+	public static void onClientTick(ClientTickEvent e) {
 		GUIEvent.onClientTick(Constants.mc.level);
 	}
 }
