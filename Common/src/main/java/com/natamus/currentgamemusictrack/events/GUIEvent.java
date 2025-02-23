6e9c01ecae42218acd60223dacb478e6fc23ddb0
Rick South <rick@natamus.com>
2025-02-23 11:54:02 +0100

    (v2.4) Improved logic when the GUI elements should be hidden.

--- Common/src/main/java/com/natamus/currentgamemusictrack/events/GUIEvent.java
+++ Common/src/main/java/com/natamus/currentgamemusictrack/events/GUIEvent.java
@@ -2,6 +2,7 @@ package com.natamus.currentgamemusictrack.events;
 
 import com.mojang.blaze3d.platform.Window;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.natamus.collective.functions.GUIFunctions;
 import com.natamus.currentgamemusictrack.config.ConfigHandler;
 import com.natamus.currentgamemusictrack.data.Constants;
 import com.natamus.currentgamemusictrack.data.Variables;
@@ -15,7 +16,7 @@ import java.awt.*;
 
 public class GUIEvent {
 	public static void renderOverlay(GuiGraphics guiGraphics, float tickDelta) {
-		if (Constants.mc.options.renderDebug) {
+		if (GUIFunctions.shouldHideGUI()) {
 			return;
 		}
 
