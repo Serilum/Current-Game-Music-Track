d0a438028741d29e7cdeb73f048adb838aa9c078
Rick South <rick@natamus.com>
2025-06-19 04:00:13 +0200

    Current Game Music Track source code for 1.21.6.

---
+++ Forge/src/main/java/com/natamus/currentgamemusictrack/forge/events/ForgeGUIEvent.java
@@ -0,0 +1,23 @@
+package com.natamus.currentgamemusictrack.forge.events;
+
+import com.natamus.currentgamemusictrack.data.Constants;
+import com.natamus.currentgamemusictrack.events.GUIEvent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+
+import java.lang.invoke.MethodHandles;
+
+public class ForgeGUIEvent {
+	public static void registerEventsInBus() {
+		// BusGroup.DEFAULT.register(MethodHandles.lookup(), ForgeGUIEvent.class);
+
+		TickEvent.ClientTickEvent.Post.BUS.addListener(ForgeGUIEvent::onClientTick);
+	}
+
+	@SubscribeEvent
+	public static void onClientTick(TickEvent.ClientTickEvent.Post e) {
+		GUIEvent.onClientTick(Constants.mc.level);
+	}
+}
